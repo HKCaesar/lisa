6c1f36f1c467ab6f87aca450de63ee70ed568933
Sebastian <sebastian.lehmann@ufz.de>
2015-11-26 17:17:41 +0100

    rice encoding

---
+++ src/file/bitio.h
@@ -0,0 +1,64 @@
+#ifndef BITIO
+#define BITIO
+
+class BitBuffer {
+  public:
+      BitBuffer(uint8_t *buffer)
+      :pbuf(buffer)
+      {
+        bitbuf=bitcnt=bytes_processed=0;
+      }
+      inline void PutBit(int bit) {
+        bitcnt++;
+        bitbuf=bitbuf|(bit<<(8-bitcnt));
+        if (bitcnt==8) {
+           *pbuf++=bitbuf;
+           bitcnt=bitbuf=0;
+           bytes_processed++;
+        }
+      }
+      inline int GetBit() {
+        if (bitcnt==0) {
+          bitbuf=*pbuf++;
+          bitcnt=8;
+          bytes_processed++;
+        }
+        bitcnt--;
+        return (bitbuf>>bitcnt)&1;
+      }
+      void PutBits(int val,int nbits) { // put nbits going from low to high
+        for (int k=0;k<nbits;k++) PutBit( (val>>k)&1);
+      }
+      int GetBits(int nbits) { // get nbits going from low to high
+        int val=0;
+        for (int k=0;k<nbits;k++) val|=(GetBit()<<k);
+        return val;
+      }
+      static int EstimateK(int N,int A) { // doesn't handle overflows correctly
+        int k;
+        for (k=0;(N<<k)<A;k++);
+        return k;
+      }
+      void PutRice(int val,int k) { // write varible length rice code with param k<32
+         int q=val>>k;
+         for (int i=0;i<q;i++) PutBit(1);
+         PutBit(0);
+         if (k) PutBits(val&((1<<k)-1),k);
+      }
+      int GetRice(int k)
+      {
+         int q=0,r=0;
+         while (GetBit()) q++;
+         if (k) r=GetBits(k);
+         return (q<<k)+r;
+      }
+      int GetBytesProcessed(){return bytes_processed;};
+      void Flush() {
+        if (bitcnt) {*pbuf++=bitbuf;bytes_processed++;};
+      }
+  private:
+    int bitbuf,bitcnt,bytes_processed;
+    uint8_t *pbuf;
+};
+
+#endif // BITIO
